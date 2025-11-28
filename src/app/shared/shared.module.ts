47027f621d068faf09b63f7b03300a27266b47ee
Teresa Torres <ttorrestorre@uoc.edu>
2025-11-28 12:59:46 +0100

    ejercicio8 seguimos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { PostCardComponent } from './components/post-card/post-card.component';
 import { InputTextComponent } from './components/form-controls/input-text/input-text.component';
 import { InputEmailComponent } from './components/form-controls/input-email/input-email.component';
 import { InputPasswordComponent } from './components/form-controls/input-password/input-password.component';
 import { InputDateComponent } from './components/form-controls/input-date/input-date.component';
-
 
 @NgModule({
     declarations: [
